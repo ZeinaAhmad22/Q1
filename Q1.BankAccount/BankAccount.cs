7fdd06f38b7c3e69b7cbe1a70d277e0a7157cb0b
Admin <Admin@DESKTOP-FCV4N15>
2026-08-30 10:23:17 +0300

    Add project files.

---
+++ Q1.BankAccount/BankAccount.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Q1.BankAccount
+{
+    public class BankAccount
+    {
+        public string AccountNumber {  get; set; }
+        public decimal Balance {  get;private set; }//only assigned from bank account class
+        public BankAccount(string accountNumber, decimal balance = 0)//0 is the default
+        {
+            this.AccountNumber = accountNumber;
+            this .Balance = balance;
+        }
+        public void Deposit(decimal amount)
+        {
+            if (!IsValidDeposit(amount))//checks if the number written is positive
+            {
+                Console.WriteLine("Amount must be positive");
+                return;
+            }
+            Balance += amount;//adds amount to balance
+
+        }
+        public void Withdraw(decimal amount) {
+            if (!IsValidWithdrawl(amount)) //checks if the balance is enough and postitive
+
+
+            {
+                Console.WriteLine("Insufficient Balance");
+                return;
+            }
+            Balance-= amount;//removes amount from balance
+        }
+        private bool IsValidDeposit(decimal amount) {
+            return amount > 0;
+
+        }
+        private bool IsValidWithdrawl(decimal amount) {
+            return amount > 0 && amount <= Balance;
+        }
+        public override string ToString() {
+            return $"Account Number:{AccountNumber}" +
+                    $"\nBalance:${Math.Round(Balance, 2):NO}";
+        }
+          
+
+    }
+}
---
+++ Q1.BankAccount/Program.cs
@@ -0,0 +1,16 @@
+﻿using Q1.BankAccount;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BankAccount account = new BankAccount("123987", 100);
+
+        account.Deposit(50);//new balace=150
+        account.Withdraw(200);//insufficient balance
+        account.Withdraw(90);//new balance=60
+        account.Deposit(-5);//amount must be positive
+        Console.WriteLine(account.ToString());
+
+    }
+}
